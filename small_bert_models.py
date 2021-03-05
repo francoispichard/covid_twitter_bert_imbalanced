148410430cd9360366480b77c71f388b4854a9af
francoispichard <francois.pichard@protonmail.ch>
2021-03-05 14:11:21 +0000

    Rename file

--- small_bert_models.py
+++ small_bert_models.py
@@ -303,18 +303,16 @@ def classifier_model(bert_config,
         dropout_rate=bert_config.hidden_dropout_prob,
         initializer=initializer), bert_encoder
 
-  input_word_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32, name='input_word_ids')
-  input_mask = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32, name='input_mask')
-  input_type_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32, name='input_type_ids')
+  input_word_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32)
+  input_mask = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32)
+  input_type_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32)
   
   bert_model = hub.KerasLayer(hub_module_url, trainable=hub_module_trainable)
   
-  if hub_module_url in ['tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1']:
-      pooled_output = bert_model({'input_word_ids': input_word_ids, 'input_mask': input_mask, 'input_type_ids': input_type_ids})['pooled_output']
+  pooled_output = bert_model({'input_word_ids': input_word_ids, 'input_mask': input_mask, 'input_type_ids': input_type_ids})['pooled_output']     
+  # Positional arguments of bert_model should be passed as the above command if hub_module_url =='tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1' 
+  # As a reminder, the three positional arguments ('input_word_'ids', 'input_mask', 'input_type_ids') should be passed within a list if the following models (defined in config.py) are used: 'tensorflow/bert_en_uncased_L-24_H-1024-A_16/2', 'tensorflow/bert_multi_cased_L-12_H-768-A-12/2', 'tensorflow/bert_en_wwm_uncased_L-24_H-1024_A-16/2', 'digitalepidemiologylab/covid-twitter-bert/1'
   
-  elif hub_module_url in ['tensorflow/bert_en_uncased_L-24_H-1024-A_16/2', 'tensorflow/bert_multi_cased_L-12_H-768-A-12/2', 'tensorflow/bert_en_wwm_uncased_L-24_H-1024_A-16/2', 'digitalepidemiologylab/covid-twitter-bert/1']:
-      pooled_output, _ = bert_model([input_word_ids, input_mask, input_type_ids])
-      
   output = tf.keras.layers.Dropout(rate=bert_config.hidden_dropout_prob)(pooled_output)
 
   output = tf.keras.layers.Dense(num_labels, kernel_initializer=initializer, name='output')(output)
