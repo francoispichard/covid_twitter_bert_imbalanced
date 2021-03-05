89d5bfbf6559fa9cb75995eab4b80314fe4be55a
François Pichard <francois.pichard@epfl.ch>
2021-03-05 12:02:45 +0100

    Fixing error with the loss function

--- test_imbalanced_classification.py
+++ test_imbalanced_classification.py
@@ -4,11 +4,11 @@ import pandas as pd
 import json
 
 # def get_loss_fn(num_classes, class_weights):
+    # class_weights should be a *list* of length num_classes  
     # """Gets the classification loss function."""
-    # def classification_loss_fn(labels, logits):
-def classification_loss_fn(num_classes, class_weights, labels, logits): # logits is a 2D tensor (each row corresponds to a sentence)
+def classification_loss_fn(labels, logits):
     """Classification loss."""
-    # labels is a 1D tensor filled with numerically encoded labels(each entry corresponds to a sentence)
+    # labels is a 1D tensor filled with numerically encoded labels (each entry corresponds to a sentence)
     labels = tf.squeeze(labels)
     # one_hot_labels is a 2D tensor (each row corresponds to a sentence)
     one_hot_labels = tf.one_hot(tf.cast(labels, dtype=tf.int32), depth=num_classes, dtype=tf.float32)
@@ -16,15 +16,14 @@ def classification_loss_fn(num_classes, class_weights, labels, logits): # logits
     # log_probs is a 2D tensor (each row corresponds to a sentence)
     log_probs = tf.nn.log_softmax(logits, axis=-1)
     # Size of the minibatch
-    minibatch_size = int(tf.shape(log_probs)[0])
-    
-    # Obtain Wclass (2D tensor) by reshaping class_weights (1D tensor)
-    Wclass = tf.reshape(tf.constant(class_weights * minibatch_size, dtype=tf.float32), [len(class_weights), minibatch_size]) # Wclass is a 2D tensor that represents a "broadcasting" of class_weights
-    
+    minibatch_size = logits.shape[0]
+
     # remaining_weights is a 1D tensor with length minibatch_size and is obtained by filtering the relevant class weight for each entry in the minibatch (elementwise multiplication between one_hot_labels and Wclass, both of them being 2D tensors) 
-    remaining_weights = tf.reduce_sum(tf.cast(one_hot_labels, dtype=tf.float32) * Wclass, axis=-1)
-    
-    # Elementwise multiplication between remaining_weights (1D, length = size of minibatch) and the second term (1D tensor, length = size of minibatch)
+    remaining_weights = tf.reduce_sum(tf.cast(one_hot_labels, dtype=tf.float32) * class_weights, axis=-1)
+   
+    # Below: one_hot_labels * log_probs is a 2D tensor; the reduce_sum operation along the second dimension yields a 1D tensor (length = size of minibatch)
+
+    # Elementwise multiplication between two 1D tensors; each element corresponds to the cross-entropy loss of a particular example 
     per_example_loss = - remaining_weights * (tf.reduce_sum(tf.cast(one_hot_labels, dtype=tf.float32) * log_probs, axis=-1))
     # weights_sum is a scalar that corresponds to the sum of the class weights for each entry in the minibatch
     weights_sum = tf.reduce_sum(remaining_weights, axis=-1)
